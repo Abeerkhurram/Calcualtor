ac99529a7d935270f50d358d81b97507c6b49fb0
Abeer.K <91427896+Abeerkhurram@users.noreply.github.com>
2023-04-12 12:16:28 +0500

    calculator

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.calculator;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button b;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
